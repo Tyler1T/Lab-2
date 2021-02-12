e2836c078191ac04f025d0a52ebe7f9d85d069d0
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-02-12 17:01:05 -0600

    Added more comments to the ADD command

--- src/isa.h
+++ src/isa.h
@@ -46,6 +46,11 @@
     19:16 - Rd (4 bits)
     15:12 - Rn (4 bits)
     11:0 - Src2 (12 bits)
+            Src2 contains shamt5, sh, and Rm
+              11:7 - shamt5, this is the amount Rm is shifted
+              6:5 - sh, this is the type of shift for Rm (i.e. <<, >>, >>>, ROR)
+              4 - this bit is always 0
+              3:0 - Rm, the second source operand
 
 */
 
@@ -55,43 +60,53 @@ int ADD (int Rd, int Rn, int Operand2, int I, int S, int CC) {
 
   //If I = 0 then the processor has to go get Operand2 from memory
   if(I == 0) {
+    /*
+      These integers apply a mask to different parts of the operand in order to
+      look at specific values
+    */
     int sh = (Operand2 & 0x00000060) >> 5;
     int shamt5 = (Operand2 & 0x00000F80) >> 7;
     int bit4 = (Operand2 & 0x00000010) >> 4;
     int Rm = Operand2 & 0x0000000F;
     int Rs = (Operand2 & 0x00000F00) >> 8;
+
+    /*This IF checks bit 4 is 1 or 0
+        1 means that this is a Register
+        0 means that this is a Register-shifted Register
+    */
     if (bit4 == 0)
+      //switch determines how Rm will be shifted
       switch (sh) {
-      case 0: cur = CURRENT_STATE.REGS[Rn] +
-	  (CURRENT_STATE.REGS[Rm] << shamt5);
-	  break;
-      case 1: cur = CURRENT_STATE.REGS[Rn] +
-	  (CURRENT_STATE.REGS[Rm] >> shamt5);
-	  break;
-      case 2: cur = CURRENT_STATE.REGS[Rn] +
-	  (CURRENT_STATE.REGS[Rm] >> shamt5);
+        case 0: cur = CURRENT_STATE.REGS[Rn] +
+  	     (CURRENT_STATE.REGS[Rm] << shamt5);
+  	    break;
+        case 1: cur = CURRENT_STATE.REGS[Rn] +
+  	     (CURRENT_STATE.REGS[Rm] >> shamt5);
+  	    break;
+        case 2: cur = CURRENT_STATE.REGS[Rn] +
+  	     (CURRENT_STATE.REGS[Rm] >> shamt5);
     	  break;
-      case 3: cur = CURRENT_STATE.REGS[Rn] +
-	      ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+        case 3: cur = CURRENT_STATE.REGS[Rn] +
+	       ((CURRENT_STATE.REGS[Rm] >> shamt5) |
                (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
-	  break;
-      }
-    else
-      switch (sh) {
-      case 0: cur = CURRENT_STATE.REGS[Rn] +
-	  (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
-	  break;
-      case 1: cur = CURRENT_STATE.REGS[Rn] +
-	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
-	  break;
-      case 2: cur = CURRENT_STATE.REGS[Rn] +
-	  (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
-	  break;
-      case 3: cur = CURRENT_STATE.REGS[Rn] +
-	      ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
-               (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
-	  break;
-      }
+	      break;
+      }else
+        //switch determines how Rm will be shifted
+        switch (sh) {
+          case 0: cur = CURRENT_STATE.REGS[Rn] +
+  	       (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+  	      break;
+          case 1: cur = CURRENT_STATE.REGS[Rn] +
+  	       (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+  	      break;
+          case 2: cur = CURRENT_STATE.REGS[Rn] +
+  	       (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+  	      break;
+          case 3: cur = CURRENT_STATE.REGS[Rn] +
+  	       ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+                 (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+  	      break;
+        }
   }
 
   /*
@@ -99,6 +114,7 @@ int ADD (int Rd, int Rn, int Operand2, int I, int S, int CC) {
     is no reason to go to memory again
   */
   if (I == 1) {
+    
     int rotate = Operand2 >> 8;
     int Imm = Operand2 & 0x000000FF;
     cur = CURRENT_STATE.REGS[Rn] + (Imm>>2*rotate|(Imm<<(32-2*rotate)));
