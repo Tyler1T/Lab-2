580aa1310447379def0a28745e5f9a81d12188a4
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-02-19 17:09:29 -0600

    Fixed part of EOR

--- src/isa.h
+++ src/isa.h
@@ -20,7 +20,7 @@
 
 #define N_N 0x80000000
 #define Z_N 0x40000000
-#define C_N 0x20000000
+#define C_N 0x20000000 // Carry
 #define V_N 0x10000000
 
 #include <stdio.h>
@@ -289,7 +289,6 @@ int AND (int Rd, int Rn, int Operand2, int I, int S, int CC){
         NEXT_STATE.CPSR |= Z_N;
     }
     return 0;
-
 }
 int ASR (char* i_);
 int B (char* i_);
@@ -298,9 +297,99 @@ int BL (char* i_);
 int CMN (char* i_);
 int CMP (char* i_);
 int EOR (int Rd, int Rn, int Operand2, int I, int S, int CC){
-  int gateOne = !AND(Rd, Rn, Operand2, I, S, CC);
-  int gateTwo = ORR(Rd, Rn, Operand2, I, S, CC);
-  return gateOne & gateTwo;
+
+      int cur = 0;
+      int nand = 0;
+      int or = 0;
+
+      //If I = 0 then the processor has to go get Operand2 from memory
+      if(I == 0) {
+        /*
+          These integers apply a mask to different parts of the operand in order to
+          look at specific values
+        */
+        int sh = (Operand2 & 0x00000060) >> 5;
+        int shamt5 = (Operand2 & 0x00000F80) >> 7;
+        int bit4 = (Operand2 & 0x00000010) >> 4;
+        int Rm = Operand2 & 0x0000000F;
+        int Rs = (Operand2 & 0x00000F00) >> 8;
+
+        /*This IF checks bit 4 is 1 or 0
+            1 means that this is a Register
+            0 means that this is a Register-shifted Register
+        */
+        if (bit4 == 0)
+          //switch determines how Rm will be shifted
+          switch (sh) {
+            case 0: nand = !(CURRENT_STATE.REGS[Rn] &
+      	     (CURRENT_STATE.REGS[Rm] << shamt5));
+              or = CURRENT_STATE.REGS[Rn] |
+              (CURRENT_STATE.REGS[Rm] << shamt5);
+              cur = nand & or;
+      	    break;
+            case 1: nand = !(CURRENT_STATE.REGS[Rn] &
+      	     (CURRENT_STATE.REGS[Rm] >> shamt5));
+              or = CURRENT_STATE.REGS[Rn] |
+              (CURRENT_STATE.REGS[Rm] >> shamt5);
+              cur = nand & or;
+      	    break;
+            //Need to finish cases 2 and 3
+            case 2: cur = CURRENT_STATE.REGS[Rn] &
+      	     (CURRENT_STATE.REGS[Rm] >> shamt5);
+        	  break;
+            case 3: cur = CURRENT_STATE.REGS[Rn] &
+    	       ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+                   (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+    	      break;
+          }else
+            //switch determines how Rm will be shifted
+            switch (sh) {
+              case 0: nand = !(CURRENT_STATE.REGS[Rn] &
+      	       (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]));
+               or = CURRENT_STATE.REGS[Rn] |
+       	       (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+               cur = nand & or;
+      	      break;
+              case 1: nand = !(CURRENT_STATE.REGS[Rn] &
+      	       (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]));
+               or = !(CURRENT_STATE.REGS[Rn] |
+       	       (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]));
+               cur= nand & or;
+      	      break;
+              // Need to finihs cases 2 and 3
+              case 2: cur = CURRENT_STATE.REGS[Rn] &
+      	       (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+      	      break;
+              case 3: cur = CURRENT_STATE.REGS[Rn] &
+      	       ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+                     (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+      	      break;
+            }
+      }
+
+      /*
+        If I = 1 then the number being added is there in the command and there
+        is no reason to go to memory again
+      */
+      if (I == 1) {
+
+        int rotate = Operand2 >> 8;
+        int Imm = Operand2 & 0x000000FF;
+        nand = !(CURRENT_STATE.REGS[Rn] & (Imm>>2*rotate|(Imm<<(32-2*rotate))));
+        or
+      }
+      NEXT_STATE.REGS[Rd] = cur;
+
+      /*
+        If S = 1 then set the condition flags
+      */
+      if (S == 1) {
+        if (cur < 0)
+          NEXT_STATE.CPSR |= N_N;
+        if (cur == 0)
+          NEXT_STATE.CPSR |= Z_N;
+      }
+      return 0;
 }
 int LDR (char* i_);
 int LDRB (char* i_);
