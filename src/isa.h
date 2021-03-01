5d6cd632d5e02049dfd5843bcd344cf321ad3226
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-03-01 15:39:56 -0600

    Finished CMP

--- src/isa.h
+++ src/isa.h
@@ -518,7 +518,7 @@ int CMN (int Rd, int Rn, int Operand2, int I, int S, int CC){
       int Imm = Operand2 & 0x000000FF;
       cur = CURRENT_STATE.REGS[Rn] - (Imm>>2*rotate|(Imm<<(32-2*rotate)));
     }
-  
+
     if (S == 1) {
       if (cur < 0)
         NEXT_STATE.CPSR |= N_N;
@@ -530,28 +530,97 @@ int CMN (int Rd, int Rn, int Operand2, int I, int S, int CC){
       }
     }
     return 0;
-}
-
+} //DONE
 int CMP (int Rd, int Rn, int Operand2, int I, int S, int CC){
 
+    int cur = 0;
 
+    //If I = 0 then the processor has to go get Operand2 from memory
+    if(I == 0) {
+      /*
+        These integers apply a mask to different parts of the operand in order to
+        look at specific values
+      */
+      int sh = (Operand2 & 0x00000060) >> 5;
+      int shamt5 = (Operand2 & 0x00000F80) >> 7;
+      int bit4 = (Operand2 & 0x00000010) >> 4;
+      int Rm = Operand2 & 0x0000000F;
+      int Rs = (Operand2 & 0x00000F00) >> 8;
 
+      /*This IF checks bit 4 is 1 or 0
+          1 means that this is a Register
+          0 means that this is a Register-shifted Register
+      */
+      if (bit4 == 0)
+        //switch determines how Rm will be shifted
+        switch (sh) {
+          case 0: cur = CURRENT_STATE.REGS[Rn] +
+    	     (CURRENT_STATE.REGS[Rm] << shamt5);
+    	    break;
+          case 1: cur = CURRENT_STATE.REGS[Rn] +
+    	     (CURRENT_STATE.REGS[Rm] >> shamt5);
+    	    break;
+          case 2: if(CURRENT_STATE.REGS[Rm] & 0x80000000 == 0)
+                    cur = CURRENT_STATE.REGS[Rm] >> shamt5;
+                  else{
+                    for(int i = 0; i < shamt5; i++)
+                          cur = (CURRENT_STATE.REGS[Rm] >> 1) + 0x80000000;
+                  }
+                  cur = CURRENT_STATE.REGS[Rn] + cur;
+      	  break;
+          case 3: cur = CURRENT_STATE.REGS[Rn] +
+  	       ((CURRENT_STATE.REGS[Rm] >> shamt5) |
+                 (CURRENT_STATE.REGS[Rm] << (32 - shamt5)));
+  	      break;
+        }else
+          //switch determines how Rm will be shifted
+          switch (sh) {
+            case 0: cur = CURRENT_STATE.REGS[Rn] +
+    	       (CURRENT_STATE.REGS[Rm] << CURRENT_STATE.REGS[Rs]);
+    	      break;
+            case 1: cur = CURRENT_STATE.REGS[Rn] +
+    	       (CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]);
+    	      break;
+            case 2: if(CURRENT_STATE.REGS[Rm] & 0x80000000 == 0)
+                      cur = CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs];
+                    else{
+                      for(int i = 0; i < CURRENT_STATE.REGS[Rs]; i++)
+                            cur = (CURRENT_STATE.REGS[Rm] >> 1) + 0x80000000;
+                    }
+                    cur = CURRENT_STATE.REGS[Rn] + cur;
+    	      break;
+            case 3: cur = CURRENT_STATE.REGS[Rn] +
+    	       ((CURRENT_STATE.REGS[Rm] >> CURRENT_STATE.REGS[Rs]) |
+                   (CURRENT_STATE.REGS[Rm] << (32 - CURRENT_STATE.REGS[Rs])));
+    	      break;
+          }
+    }
 
+    /*
+      If I = 1 then the number being added is there in the command and there
+      is no reason to go to memory again
+    */
+    if (I == 1) {
+      int rotate = Operand2 >> 8;
+      int Imm = Operand2 & 0x000000FF;
+      cur = CURRENT_STATE.REGS[Rn] + (Imm>>2*rotate|(Imm<<(32-2*rotate)));
+    }
+  
+    /*
+      If S = 1 then set the condition flags
+    */
+    if (S == 1) {
+      if (cur < 0)
+        NEXT_STATE.CPSR |= N_N;
+      if (cur == 0)
+        NEXT_STATE.CPSR |= Z_N;
+      if(cur > 0xffffffff){
+        NEXT_STATE.CPSR |= V_N;
+        NEXT_STATE.CPSR |= C_N;
 
-
-  /*
-    If S = 1 then set the condition flags
-  */
-  if (S == 1) {
-    if (cur < 0)
-      NEXT_STATE.CPSR |= N_N;
-    if (cur == 0)
-      NEXT_STATE.CPSR |= Z_N;
-    if(cur > 0xffffffff)
-      NEXT_STATE.CPSR |= V_N;
-      NEXT_STATE.CPSR |= C_N;
-  }
-  return 0;
+      }
+    }
+    return 0;
 }
 
 
